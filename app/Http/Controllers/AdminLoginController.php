4bf078d49a7a1d5c771359b7a807ffe9793a169f
Web Dev <web.2019.dev@gmail.com>
2022-09-14 13:28:11 +0545

    email setup

--- app/Http/Controllers/AdminLoginController.php
+++ app/Http/Controllers/AdminLoginController.php
@@ -4,9 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Admin;
+use App\Mail\Websitemail;
+use Hash;
+use Auth;
+
 class AdminLoginController extends Controller
 {
     public function index(){
+      $pass = Hash::make('1234');
+
+
       return view('admin.login');
     }
 
