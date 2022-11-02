67c5a592977b293aeb39af86eca1c1d6e17d5d5e
Web Dev <web.2019.dev@gmail.com>
2022-11-02 18:55:47 +0545

    updated to bs5

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -9,7 +9,7 @@
 
     <ul class="sidebar-menu">
 
-      <li class="active"><a class="nav-link" href="{{ route('admin_home') }}"><i class="fas fa-hand-point-right"></i>
+      <li class="active"><a class="nav-link" href="{{ route('admin_home') }}" data-bs-toggle="tooltip" data-bs-placement = "right" data-bs-title = "Dashboard"><i class="fas fa-hand-point-right"></i>
           <span>Dashboard</span></a></li>
 
       <li class="nav-item dropdown active">
@@ -20,16 +20,16 @@
         </ul>
       </li>
 
-      <li class=""><a class="nav-link" href="setting.html"><i class="fas fa-hand-point-right"></i>
+      <li class=""><a class="nav-link" href="setting.html" data-bs-toggle="tooltip" data-bs-placement = "right" data-bs-title = "Setting"><i class="fas fa-hand-point-right"></i>
           <span>Setting</span></a></li>
 
-      <li class=""><a class="nav-link" href="form.html"><i class="fas fa-hand-point-right"></i> <span>Form</span></a>
+      <li class=""><a class="nav-link" href="form.html" data-bs-toggle="tooltip" data-bs-placement = "right" data-bs-title = "Form"><i class="fas fa-hand-point-right"></i> <span>Form</span></a>
       </li>
 
-      <li class=""><a class="nav-link" href="table.html"><i class="fas fa-hand-point-right"></i> <span>Table</span></a>
+      <li class=""><a class="nav-link" href="table.html" data-bs-toggle="tooltip" data-bs-placement = "right" data-bs-title = "Table"><i class="fas fa-hand-point-right"></i> <span>Table</span></a>
       </li>
 
-      <li class=""><a class="nav-link" href="invoice.html"><i class="fas fa-hand-point-right"></i>
+      <li class=""><a class="nav-link" href="invoice.html" data-bs-toggle="tooltip" data-bs-placement = "right" data-bs-title = "Invoice"><i class="fas fa-hand-point-right"></i>
           <span>Invoice</span></a></li>
 
     </ul>
